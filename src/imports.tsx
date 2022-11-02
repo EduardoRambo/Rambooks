989669d493d74afcad8fda78f9c165c819802491
EduardoRambo <eduardorambolima@gmail.com>
2022-11-02 14:41:14 -0300

    Changing imports

--- src/imports.tsx
+++ src/imports.tsx
@@ -1,12 +1,12 @@
 export const Images = {
-    quadrinho1: ('/images/quadrinho1.PNG'),
-    quadrinho2: ('/images/quadrinho2.PNG'),
-    quadrinho3: ('/images/quadrinho3.PNG'),
-    quadrinho4: ('/images/quadrinho4.PNG'),
-    quadrinho5: ('/images/quadrinho5.PNG'),
-    sobrenos: ('/images/sobrenos.PNG'),
-    icone: ('/images/icone.PNG'),
-    logo: ('/images/logo.PNG'),
-    background: ('/images/background.PNG'),
-    bolha: ('/images/bolha.PNG')
+    quadrinho1: ('/images/quadrinho1.png'),
+    quadrinho2: ('/images/quadrinho2.png'),
+    quadrinho3: ('/images/quadrinho3.png'),
+    quadrinho4: ('/images/quadrinho4.png'),
+    quadrinho5: ('/images/quadrinho5.png'),
+    sobrenos: ('/images/sobrenos.png'),
+    icone: ('/images/icone.png'),
+    logo: ('/images/logo.png'),
+    background: ('/images/background.png'),
+    bolha: ('/images/bolha.png')
 }
