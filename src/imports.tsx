3fd663a44d1e7c619ad479410c10a58389f1ecc2
EduardoRambo <eduardorambolima@gmail.com>
2022-11-02 14:48:09 -0300

    Changing URLS on images

--- src/imports.tsx
+++ src/imports.tsx
@@ -1,12 +1,12 @@
 export const Images = {
-    quadrinho1: ('/images/quadrinho1.png'),
-    quadrinho2: ('/images/quadrinho2.png'),
-    quadrinho3: ('/images/quadrinho3.png'),
-    quadrinho4: ('/images/quadrinho4.png'),
-    quadrinho5: ('/images/quadrinho5.png'),
-    sobrenos: ('/images/sobrenos.png'),
-    icone: ('/images/icone.png'),
-    logo: ('/images/logo.png'),
-    background: ('/images/background.png'),
-    bolha: ('/images/bolha.png')
+    quadrinho1: ('Rambooks/images/quadrinho1.png'),
+    quadrinho2: ('Rambooks/images/quadrinho2.png'),
+    quadrinho3: ('Rambooks/images/quadrinho3.png'),
+    quadrinho4: ('Rambooks/images/quadrinho4.png'),
+    quadrinho5: ('Rambooks/images/quadrinho5.png'),
+    sobrenos: ('Rambooks/images/sobrenos.png'),
+    icone: ('Rambooks/images/icone.png'),
+    logo: ('Rambooks/images/logo.png'),
+    background: ('Rambooks/images/background.png'),
+    bolha: ('Rambooks/images/bolha.png')
 }
