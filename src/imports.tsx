79fb782ea53b7799b3d7785436d9016f48956812
EduardoRambo <eduardorambolima@gmail.com>
2022-11-02 14:38:03 -0300

    Changed Jpg/Jpeg to PNG

--- src/imports.tsx
+++ src/imports.tsx
@@ -1,12 +1,12 @@
 export const Images = {
-    quadrinho1: ('/images/quadrinho1.JPG'),
-    quadrinho2: ('/images/quadrinho2.JPG'),
-    quadrinho3: ('/images/quadrinho3.JPG'),
-    quadrinho4: ('/images/quadrinho4.JPG'),
-    quadrinho5: ('/images/quadrinho5.JPEG'),
-    sobrenos: ('/images/sobrenos.JPG'),
+    quadrinho1: ('/images/quadrinho1.PNG'),
+    quadrinho2: ('/images/quadrinho2.PNG'),
+    quadrinho3: ('/images/quadrinho3.PNG'),
+    quadrinho4: ('/images/quadrinho4.PNG'),
+    quadrinho5: ('/images/quadrinho5.PNG'),
+    sobrenos: ('/images/sobrenos.PNG'),
     icone: ('/images/icone.PNG'),
     logo: ('/images/logo.PNG'),
     background: ('/images/background.PNG'),
-    bolha: ('/images/bolha.JPG')
+    bolha: ('/images/bolha.PNG')
 }
